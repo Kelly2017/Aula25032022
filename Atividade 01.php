dbb6806aa32e7f8ebb224c4297703ea145f53343
Nayara78 <nayravive0407@gmail.com>
2022-03-25 21:00:38 -0400

    REsoluçao atividade 01

---
+++ Atividade 01.php
@@ -0,0 +1,25 @@
+<html>
+    
+    <read>
+        <title>Atividade Pré Avaliacao</title>    
+    </read>
+
+    <body>
+        <h1>Calcular Aumento de Salario</h1>
+
+
+    
+        <?php 
+
+         $salario = 1500.00;
+         $aumento = ($salario * 15) /100;
+         $novosalario = $salario + $aumento;
+
+
+       echo "O Salário de João Irá de R$ " .$salario. " Para R$ " .$novosalario ;
+        
+
+        ?>   
+
+   </body>
+</html>
