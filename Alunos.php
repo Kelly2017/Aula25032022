b61afdca81151daeacbeb9997be6da921bd2334b
Nayara78 <nayravive0407@gmail.com>
2022-03-25 21:20:41 -0400

    Exercicio 0,2 Alunos Turma

---
+++ Alunos.php
@@ -0,0 +1,55 @@
+<html>
+    
+    <read>
+        <title>Nome dos Alunos</title>    
+    </read>
+
+    <body>
+        <h1>Exibir nmome dos alunos</h1>
+
+
+    
+        <?php 
+
+         $aluno1 = "Kelly";
+         $aluno2 = "Alan";
+         $aluno3 = "Jonathan";
+         $aluno4 = "Manoel";
+         $aluno5 = "Eduardo";
+         $aluno6 = "Cebolinha - Iago";
+         $aluno7 = "Filipe - Filho do prof Hailton";
+         $aluno8 = "Mozart";
+         $aluno9 = "Bruno";
+         $aluno10 = "Victor";
+         $aluno11 = "Walisson";
+         $aluno12 = "Enio";
+         $aluno13 = "Patrycia";
+         $aluno14 = "Igor";
+         $aluno15 = "Matheus";
+         $aluno16 = "Brunna";
+         $aluno17 = "Lucas";
+
+       echo "Alunos Da Turma de Desenvolvimento Web Com Framwork </br>";
+       echo "Aluno 01: ".$aluno1. "</br>";
+       echo "Aluno 02: ".$aluno2. "</br>";
+       echo "Aluno 03: ".$aluno3. "</br>";
+       echo "Aluno 04: ".$aluno4. "</br>";
+       echo "Aluno 05: ".$aluno5. "</br>";
+       echo "Aluno 06: ".$aluno6. "</br>";
+       echo "Aluno 07: ".$aluno7. "</br>";
+       echo "Aluno 08: ".$aluno8. "</br>";
+       echo "Aluno 09: ".$aluno9. "</br>";
+       echo "Aluno 10: ".$aluno10. "</br>";
+       echo "Aluno 11: ".$aluno11. "</br>";
+       echo "Aluno 12: ".$aluno12. "</br>";
+       echo "Aluno 13: ".$aluno13. "</br>";
+       echo "Aluno 14: ".$aluno14. "</br>";
+       echo "Aluno 15: ".$aluno15. "</br>";
+       echo "Aluno 16: ".$aluno16. "</br>";
+       echo "Aluno 17: ".$aluno17. "</br>";
+        
+
+        ?>   
+
+   </body>
+</html>
